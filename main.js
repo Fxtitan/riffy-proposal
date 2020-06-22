b3c79d2623c98509450d30866c6f4592f8b0d501
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-06-22 11:26:55 -0400

    made some changes

--- main.js
+++ main.js
@@ -9,15 +9,15 @@ const budget = getInput(1);
 //Node functionality//
 switch(traits + "|" + budget) {
     case undefined: return console.log('You have failed to pick a trait from the list.');
-    case 'simple|500': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
-    case 'elegant|3000': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
-    case 'bubbly|10000': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
-    case 'intelligent|600': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
-    case 'kind|5000': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
-    case 'conservative|750': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
-    case 'confident|12000': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
-    case 'creative|1001': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
-    case 'reliable|20': return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case `simple|${budget}`: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case 'elegant|' + budget: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case 'bubbly|' + budget: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case 'intelligent|' + budget: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case 'kind|' + budget: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case 'conservative|' + budget: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case 'confident|' + budget: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case 'creative|' + budget: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
+    case `reliable|${budget}`: return console.log(personalityTrait(traits)) + console.log(timeIsMoney(budget));
     default:   return console.log('sigh');
 }
 
--- personality-trait.js
+++ personality-trait.js
@@ -1,14 +1,15 @@
 const personalityTrait = (trait) => {
     switch(trait) {
         case 'simple': return 'The color you should choose is white.' ;
-        case 'elegant': return ' The color you should choose is black.';
-        case 'bubbly': return ' The color you should choose is red.';
-        case 'intelligent': return ' The color you should choose is red.';
-        case 'kind': return ' The color you should choose is green.';
-        case 'conservative': return ' The color you should choose is grey.';
-        case 'confident': return ' The color you should choose is yellow.';
-        case 'creative': return ' The color you should choose is orange.';
-        case 'reliable': return ' The color you should choose is brown.';
+        case 'elegant': return 'The color you should choose is black.';
+        case 'bubbly': return 'The color you should choose is red.';
+        case 'intelligent': return 'The color you should choose is blue.';
+        case 'kind': return 'The color you should choose is green.';
+        case 'conservative': return 'The color you should choose is grey.';
+        case 'confident': return 'The color you should choose is yellow.';
+        case 'creative': return 'The color you should choose is orange.';
+        case 'reliable': return 'The color you should choose is brown.';
+        default: return 'You need to choose a trait.' 
     }
 }
 
